9cabdea0f414f707e75fd328029e119b77a0ddf2
Moamen Ragab <moamenragab66@gmail.com>
2026-05-16 00:19:33 +0300

    add appcontext

---
+++ Data/Entities/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Personal_Blogging_Platform.Data.Entities
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
